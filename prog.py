6781c75e508646e21125bca0315329814dceb59b
AndyRem1x <115189609+AndyRem1x@users.noreply.github.com>
2023-08-20 15:24:15 +0300

    Delete prog.py

--- prog.py
+++
@@ -1,8 +0,0 @@
-class Greeting:
-    def __init__(self, text):
-        self.text = text
-    def print_greeting(self):
-        print(self.text)
-
-quote = Greeting("Hello! Study python!")
-quote.print_greeting()
