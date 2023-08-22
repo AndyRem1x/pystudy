aa38e040cba955d6ead561dba8e395e3eec9d78f
AndyRem1x <115189609+AndyRem1x@users.noreply.github.com>
2023-08-22 23:14:48 +0300

    Upgraded code of 3rd task

--- btype_task/task3.py
+++ btype_task/task3.py
@@ -1,8 +1,27 @@
-from random import randint
+from random import randint, choice
 a = randint(1, 20)
 b = randint(1, 20)
-answer = input(f"{a} + {b} equals? ")
-if int(answer) == (a + b):
+operators = ["+", "-", "*", "/", "**", "//", "%"]
+answer = 0
+selected = choice(operators)
+guess = input("If the answer of expression is decimal,"
+              " round the number to two decimal places!"
+              f"\n{a} {selected} {b} equals?: ")
+if selected == "+":
+    answer = a + b
+elif selected == "-":
+    answer = a - b
+elif selected == "*":
+    answer = a * b
+elif selected == "/":
+    answer = a / b
+elif selected == "**":
+    answer = a ** b
+elif selected == "//":
+    answer = a // b
+elif selected == "%":
+    answer = a % b
+if float(guess) == round(float(answer), 2):
     print("You are right!")
 else:
     print("You are wrong!")
