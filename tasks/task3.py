31d1572755dc5d6b0a9c5ee5a24e02c8bd077099
AndyRem1x <115189609+AndyRem1x@users.noreply.github.com>
2023-08-23 19:21:49 +0300

    Added completed tasks

---
+++ tasks/task3.py
@@ -0,0 +1,3 @@
+print("#"*9+"\n#\t#\n#\t#\n#\t#\n"+"#"*9)  # \t is 8 spaces by default settings of python interpreter
+print()#space between 2 letters
+print("#\t#\n#\t#\n"+"#"*9+"\n#\t#\n#\t#") # \t is 8 spaces by default settings of python interpreter
