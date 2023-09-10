ef5c437ce186af76de2572e046dd15346ce65957
AndyRem1x <115189609+AndyRem1x@users.noreply.github.com>
2023-09-10 20:47:49 +0300

    Add files with completed tasks

--- lists_task/ltask1.py
+++ lists_task/ltask1.py
@@ -1,1 +1,8 @@
-
+from random import randint
+lst = []
+length = 10
+counter = 0
+while counter < length:
+    lst.append(randint(1, 20))
+    counter += 1
+print(max(lst))
--- lists_task/ltask3.py
+++ lists_task/ltask3.py
@@ -1,1 +1,8 @@
-
+list_1 = list(range(1, 101))
+list_2 = []
+counter = 0
+while counter < len(list_1):
+    if list_1[counter] % 7 == 0 and list_1[counter] % 5 != 0:
+        list_2.append(list_1[counter])
+    counter += 1
+print(list_2)
