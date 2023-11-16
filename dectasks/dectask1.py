8e65242428b6d28cb05ac3246a4cdcd6b26e8dc5
AndyRem1x <115189609+AndyRem1x@users.noreply.github.com>
2023-11-17 01:34:23 +0200

    Add files with completed tasks

--- dectasks/dectask1.py
+++ dectasks/dectask1.py
@@ -1,1 +1,19 @@
+def logger(func):
+    def wrapper(*args):
+        all_args = [str(arg) for arg in args]
+        print(f"{func.__name__} called with " + ", ".join(all_args))
+    return wrapper
 
+
+@logger
+def add(x, y):
+    return x + y
+
+
+@logger
+def square_all(*args):
+    return [arg ** 2 for arg in args]
+
+
+add(4, 5)
+square_all(5, 6, 7, 8, 9)
--- dectasks/dectask2.py
+++ dectasks/dectask2.py
@@ -1,1 +1,18 @@
+def stop_words(words: list):
 
+    def wrapper(func):
+        def second_wrapper(name):
+            slogan = func(name)
+            for word in words:
+                slogan = slogan.replace(word, "*")
+            return slogan
+        return second_wrapper
+    return wrapper
+
+
+@stop_words(['pepsi', 'BMW'])
+def create_slogan(name: str) -> str:
+    return f"{name} drinks pepsi in his brand new BMW!"
+
+
+assert create_slogan("Steve") == "Steve drinks * in his brand new *!"
