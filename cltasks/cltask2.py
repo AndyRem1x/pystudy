138c74cee066294464c6d52f3274a693e078d789
AndyRem1x <115189609+AndyRem1x@users.noreply.github.com>
2023-11-17 03:18:05 +0200

    Add files with completed tasks

--- cltasks/cltask2.py
+++ cltasks/cltask2.py
@@ -1,1 +1,14 @@
+class Dog:
+    age_factor = 7
 
+    def __init__(self, age):
+        self.age = age
+
+    def human_age(self):
+        print(f"{self.age} dog's years is {self.age * self.age_factor} years in human equivalent")
+
+
+first_dog = Dog(7)
+first_dog.human_age()
+second_dog = Dog(5)
+second_dog.human_age()
